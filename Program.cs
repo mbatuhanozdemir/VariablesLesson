0b2e9377eefea3ea0120f8121bdb483133f16933
mbatuhanozdemir <mbatuhanozdemir@hotmail.com>
2022-12-11 00:40:29 +0300

    yüklendi

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿ class Program
+{
+    public static void Main(string[] args)
+    {
+        string tarih = DateTime.Now.ToString("HH:mm");
+        Console.WriteLine(tarih);
+
+        string t1st; 
+
+        int degisken = 5;
+        string degisken = "merhaba";
+        Console.WriteLine(degisken);
+
+         
+    }
+}
